056c52f1efb13dd690025f60e0fa12d63e012a46
Rasmus <rasmus.kallqvist@gmail.com>
2022-05-01 22:41:05 +0200

    Make loop

--- src/main/java/com/calc/Main.java
+++ src/main/java/com/calc/Main.java
@@ -5,15 +5,26 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Stack<String> expr = new Stack<>();
 
-        System.out.print("enter expression: ");
-        for (String item : scanner.nextLine().split(" ")) {
-            expr.push(item);
+        while (true) {
+            System.out.print("> ");
+            String input = scanner.nextLine();
+            if (input.equals("quit")) {
+                break;
+            }
+            Stack<String> expr = splitIntoStack(input);
+            int output = StackCalculator.evaluate(expr);
+            System.out.println(output);
         }
-        int output = StackCalculator.evaluate(expr);
-        System.out.println("output = "+ output);
 
         scanner.close();
+    }
+
+    static Stack<String> splitIntoStack(String str) {
+        Stack<String> stack = new Stack<>();
+        for (String word : str.split(" ")) {
+            stack.push(word);
+        }
+        return stack;
     }
 }
